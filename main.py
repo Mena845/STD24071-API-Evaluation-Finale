75d5f7309ed320aec409f320d72705ca5b69d4c3
Mena845 <tsikiniainatsilavina@gmail.com>
2025-09-03 09:50:53 +0300

    Add a method to post phone's list

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
@@ -8,3 +10,20 @@ def health():
     return "Ok"
 
 
+class Characteristic(BaseModel):
+    ram_memory: int
+    rom_memory: int
+
+class Phone(BaseModel):
+    id: int
+    brand: str
+    model: str
+    characteristics: Characteristic
+
+phones_db: List[Phone] = []
+
+# b: POST /phones
+@app.post("/phones", status_code=201, response_model=Phone)
+def create_phone(phone: Phone):
+    phones_db.append(phone)
+    return phone
